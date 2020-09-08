6b7ba6baccd5a13ffe1d5a75e035037e7152274a
Mohammad Amir Jamil <amirjamil900@gmail.com>
2020-09-08 11:52:45 +0530

    Update loc.py

--- loc.py
+++ loc.py
@@ -1,5 +1,8 @@
 # Working but not able to generalise the concept for all the searched queries.Although getting correct for few ..
-
+# The code has been written by Mohammad Amir Jamil. 
+# Need to generalise the concept of Location Identifier. 
+# This could be used to link the KML file and Tracking System for an Individual.
+# If u need to reverse the search i.e. Query search for coordinates, that is also possible but it requires the exact Query String. 
 from geopy.geocoders import Nominatim
 geolocator = Nominatim()
 location = geolocator.geocode("Khurais Mall Riyadh")
@@ -13,4 +16,6 @@ import simplekml
 kml = simplekml.Kml()
 kml.newpoint(name="amir",coords=[(location.longitude,location.latitude)])
 kml.save("coordinate.kml")
+
+
 
